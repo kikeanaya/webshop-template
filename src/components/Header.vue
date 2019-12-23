b21603726f4b3e2c7b03206a12b2db0f0fa26d28
kikeanaya <luiseanayam@gmail.com>
2019-12-23 10:17:10 +0100

    add image slider to homepage

---
+++ src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+<div id="header-container">
+  <div id="top-container">
+    <h2>FREE SHIPPING ON ORDERS OVER 40 €</h2>
+    <input placeholder="search here">
+  </div>
+  <img id="website-logo" src="../assets/logo.png"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <a>All Items</a>
+    <router-link to="/contact">Contact</router-link>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+#header-container {
+  border-bottom: 1px solid black;
+}
+
+#website-logo {
+  height: 120px;
+}
+
+#top-container {
+  display: flex;
+  justify-content: space-between;
+  height: 30px;
+  margin: 10px;
+
+  h2 {
+    font-size: 0.7em;
+    font-weight: 100;
+    margin: 0px;
+  }
+
+  input {
+    width: 200px;
+    padding: 0px 10px;
+    border: 0px;
+    border-bottom: 1px solid lightgray;
+    outline: none;
+  }
+}
+
+#nav {
+  height: 30px;
+  width: 60%;
+  margin: 0 auto;
+  padding-top: 20px;
+  display: flex;
+  justify-content: space-evenly;
+
+  a {
+    font-weight: bold;
+    text-decoration: none;
+    color: black;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="highlights-container">
+    <img id="website-logo" :src="require(`@/assets/${images[imageCounter]}`)" :key="images[imageCounter]"/>
+    <a id="previous-icon" @click="previousClick">&#10094;</a>
+    <a id="next-icon" @click="nextClick">&#10095;</a>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      imageCounter: 0,
+      images: [
+        "image1.jpg",
+        "image2.jpg",
+        "image3.jpg",
+        "image4.jpg"
+      ]
+    }
+  },
+
+  methods: {
+    previousClick() {
+      if (this.imageCounter === 0) return
+      else this.imageCounter--;
+    },
+    nextClick() {
+      if (this.imageCounter === 3) return
+      else this.imageCounter++;
+    }
+  },
+
+  computed: {
+    selectedImage() {
+      return this.images[this.imageCounter];
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  #highlights-container {
+    margin: 10px auto;
+    height: 400px;
+    width: 50%;
+    border:  1px solid black;
+    position: relative;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  #previous-icon {
+    position: absolute;
+    left: 10px;
+    top: 185px;
+    color: white;
+    font-size: 2em;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  #next-icon {
+    position: absolute;
+    right: 10px;
+    top: 185px;
+    color: white;
+    font-size: 2em;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+</style>
