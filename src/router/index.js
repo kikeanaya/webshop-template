2be47962a7efdd0fb129358c195f64613d93c85e
kikeanaya <luiseanayam@gmail.com>
2019-12-23 12:49:25 +0100

    add catalog view

--- src/router/index.js
+++ src/router/index.js
@@ -19,8 +19,8 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/contact',
-    name: 'about',
+    path: '/catalog',
+    name: 'catalog',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,27 +1,83 @@
 <template>
-    <div>
-        <h1>Catalog</h1>
+  <div id="card-container">
+    <div class="beer-card" data-aos="fade-up">
+        <img src="../assets/beer1.png">
+        <h5>OTHER HALF GINGERBREAD</h5>
+        <h5>9 €</h5>
     </div>
+    <div class="beer-card" data-aos="fade-up">
+        <img src="../assets/beer2.png">
+        <h5>OTHER HALF FLORETS</h5>
+        <h5>6 €</h5>
+    </div>
+    <div class="beer-card" data-aos="fade-up">
+        <img src="../assets/beer3.png">
+        <h5>OTHER HALF BALACLAVA</h5>
+        <h5>8 €</h5>
+    </div>
+    <div class="beer-card" data-aos="fade-up">
+        <img src="../assets/beer4.png">
+        <h5>OTHER HALF HOP SHOWERS</h5>
+        <h5>9 €</h5>
+    </div>
+    <div class="beer-card" data-aos="fade-up">
+        <img src="../assets/beer5.png">
+        <h5>OTHER HALF CREAM ON THE INSIDE</h5>
+        <h5>7 €</h5>
+    </div>
+    <div class="beer-card" data-aos="fade-up">
+        <img src="../assets/beer6.png">
+        <h5>OTHER HALF GREEN DIAMONDS</h5>
+        <h5>6 €</h5>
+    </div>
+    <div class="beer-card" data-aos="fade-up">
+        <img src="../assets/beer7.png">
+        <h5>OTHER HALF COBBLER JAMS</h5>
+        <h5>6 €</h5>
+    </div>
+    <div class="beer-card" data-aos="fade-up">
+        <img src="../assets/beer8.png">
+        <h5>OTHER HALF MYLAR BAGS</h5>
+        <h5>8 €</h5>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'home',
+  name: 'catalog',
 
-    data() {
-        return {
-            imageCounter: 0,
-            images: [
-                image1,
-                image2,
-                image3,
-                image4
-            ]
-        }
-    },
+  data() {
+    return {
+
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+  .beer-card {
+    border: 1px solid lightgray;
+    border-radius: 3px;
+    width: 300px;
+    height: 350px;
+    text-align: left;
+    margin: 20px;
 
+    h5 {
+      margin: 0 15px;
+    }
+
+    img {
+      width: 100%;
+    }
+  }
+
+  #card-container {
+    width: 80%;
+    margin: 30px auto;
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+  }
 </style>
